f9ddd70b5328d73b21b8d79432bbf28f9f6a08a1
rajagroupvip <rajagroupvip@gmail.com>
2026-05-29 19:52:51 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ALL);
 $host="localhost"; 
-$user="jasawebkuduabiz_db"; 
-$password="jasawebkuduabiz_db"; 
-$database="jasawebkuduabiz_db"; 
+$user="seobjatg_seobjatg"; 
+$password="vuAQXZFkrSNP"; 
+$database="seobjatg_seobjatg"; 
 $conn=mysqli_connect($host,$user,$password,$database) or die(mysqli_error());
 //cek koneksi 
 if($conn){ 
